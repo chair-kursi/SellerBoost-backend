8ba75c54836fbd6a6548d5c6b4538537220a504a
Vaibhav <vkpstudyy247@gmail.com>
2021-10-07 15:04:42 +0530

    get skuTraffic avl

--- routes/skuTraffic.js
+++ routes/skuTraffic.js
@@ -7,8 +7,14 @@ const SkuTraffic = require('../models/SkuTrafficMongo');
 const clientId = getClientId(); //sir as we are getting clientId from a func, is it OK to invoke getClientId() just once here??
 
 
-router.get("/findAll", async(req, res)=>{
-    // const
+router.get("/skuTraffic", async(req, res)=>{
+    try{
+        const skuTraffic = await SkuTraffic.find({clientId: clientId})
+        res.json({data: skuTraffic, error: null});
+    }
+    catch(err){
+        res.status(400).json({message: err});
+    }
 })
 
 
