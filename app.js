a9bd023824ecd0c6302cf9ca62e93fd86214ce0c
Vaibhav <vkpstudyy247@gmail.com>
2021-10-07 15:10:22 +0530

    add status on dash

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv/config");
+const dotenv = require('dotenv')
+dotenv.config({path:__dirname+'/.env'});
+
+// require("dotenv/config");
 const app = express();
 const port = process.env.PORT || 3002;
 const os = require("os")
